219bdcf6338e51fefaf1e5486b9ae951568137fe
trandangduat <duattran.05@gmail.com>
2025-06-05 19:39:41 +0700

    improve LayersPane and HistoryPane performance by wrapping Pane item rows in React.memo

--- components/home/history-pane.tsx
+++ components/home/history-pane.tsx
@@ -1,7 +1,79 @@
 import { cn } from "@/lib/utils";
 import { ChevronUp, Eye, EyeOff, Pin, PinOff, Plus, Trash2 } from "lucide-react";
-import { useContext, useEffect, useRef } from "react";
+import { memo, useCallback, useContext, useEffect, useRef } from "react";
 import { HistoryContext } from "@/app/page";
+import { Action } from "@/types/history-stack";
+
+const HistoryRowItem = memo(({
+  action,
+  index,
+  isAtPresent,
+  isInTheFuture,
+  isInThePast,
+  onClick
+} : {
+  action: Action,
+  index: number,
+  isAtPresent: boolean,
+  isInTheFuture: boolean,
+  isInThePast: boolean,
+  onClick: any
+}) => {
+  return (
+    <div key={index} className="border-b border-slate-500">
+      <button
+        className={cn(
+          "bg-slate-700 p-2 w-full flex flex-row items-center gap-2 cursor-pointer text-sm",
+          isAtPresent && "bg-slate-800",
+          isInTheFuture && "opacity-40",
+          isInThePast && "opacity-100"
+        )}
+        onClick={() => onClick(index)}
+      >
+        {action.type === "NEW_LAYER" ? (
+          <>
+            <Plus size={16} />
+            <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
+          </>
+        ) : action.type === "DELETE_LAYER" ? (
+          <>
+            <Trash2 size={16} />
+            <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
+          </>
+        ) : action.type === "PIN_LAYER" ? (
+          <>
+            <Pin size={16} />
+            <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
+          </>
+        ) : action.type === "UNPIN_LAYER" ? (
+          <>
+            <PinOff size={16} />
+            <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
+          </>
+        ) : action.type === "HIDE_LAYER" ? (
+          <>
+            <EyeOff size={16} />
+            <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
+          </>
+        ) : action.type === "UNHIDE_LAYER" ? (
+          <>
+            <Eye size={16} />
+            <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
+          </>
+        ) : action.type === "REORDER_LAYER" ? (
+          <>
+            <ChevronUp size={16} />
+            <p>{action.type} - {action.oldIndex} to {action.newIndex}</p>
+          </>
+        ) : (
+          <>
+            <p>{action.type}</p>
+          </>
+        )}
+      </button>
+    </div>
+  )
+});
 
 export default function HistoryPane() {
   const {
@@ -19,74 +91,33 @@ export default function HistoryPane() {
     });
   }, [slideHistory]);
 
+  const handleSelectHistoryItem = useCallback((index: number) => {
+    let diff:number = index - slideHistory.currentIndex;
+    while (diff > 0) {
+      redo();
+      diff--;
+    }
+    while (diff < 0) {
+      undo();
+      diff++;
+    }
+  }, [slideHistory, undo, redo]);
+
   return (
   <>
     <p className="text-white text-2xl m-2 mb-4">History</p>
     <div className="h-full overflow-y-auto" ref={historyBoardRef}> {/* Add h-full here */}
-    {slideHistory.actions.map((action, index) => (
-      <div key={index} className="border-b border-slate-500">
-        <button
-          className={cn(
-            "bg-slate-700 p-2 w-full flex flex-row items-center gap-2 cursor-pointer text-sm",
-            slideHistory.currentIndex === index && "bg-slate-800",
-            slideHistory.currentIndex < index && "opacity-40",
-            slideHistory.currentIndex > index && "opacity-100"
-          )}
-          onClick={() => {
-            let diff:number = index - slideHistory.currentIndex;
-            while (diff > 0) {
-              redo();
-              diff--;
-            }
-            while (diff < 0) {
-              undo();
-              diff++;
-            }
-          }}
-        >
-          {action.type === "NEW_LAYER" ? (
-            <>
-              <Plus size={16} />
-              <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
-            </>
-          ) : action.type === "DELETE_LAYER" ? (
-            <>
-              <Trash2 size={16} />
-              <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
-            </>
-          ) : action.type === "PIN_LAYER" ? (
-            <>
-              <Pin size={16} />
-              <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
-            </>
-          ) : action.type === "UNPIN_LAYER" ? (
-            <>
-              <PinOff size={16} />
-              <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
-            </>
-          ) : action.type === "HIDE_LAYER" ? (
-            <>
-              <EyeOff size={16} />
-              <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
-            </>
-          ) : action.type === "UNHIDE_LAYER" ? (
-            <>
-              <Eye size={16} />
-              <p>{action.type} - {action.layer!.uuid.slice(0, 5)}</p>
-            </>
-          ) : action.type === "REORDER_LAYER" ? (
-            <>
-              <ChevronUp size={16} />
-              <p>{action.type} - {action.oldIndex} to {action.newIndex}</p>
-            </>
-          ) : (
-            <>
-              <p>{action.type}</p>
-            </>
-          )}
-        </button>
-      </div>
-    ))}
+      {slideHistory.actions.map((action, index) => (
+        <HistoryRowItem
+          key={index}
+          action={action}
+          index={index}
+          isAtPresent={slideHistory.currentIndex === index}
+          isInTheFuture={slideHistory.currentIndex < index}
+          isInThePast={slideHistory.currentIndex > index}
+          onClick={handleSelectHistoryItem}
+        />
+      ))}
     </div>
   </>
   )
--- components/home/layers-pane.tsx
+++ components/home/layers-pane.tsx
@@ -1,7 +1,7 @@
 import { ArrowLayer, CircleLayer, Layer, RectLayer } from "@/types/layer";
 import { cn } from "@/lib/utils";
 import { ArrowRight, ChevronDown, ChevronUp, Circle, Eye, EyeOff, Pin, PinOff, Square, Trash2 } from "lucide-react";
-import { JSX, useContext, useEffect, useRef, useState } from "react";
+import { JSX, memo, useCallback, useContext, useEffect, useMemo, useRef, useState } from "react";
 import { HistoryContext, LayersContext, PresentationContext } from "@/app/page";
 import { DeleteLayerAction } from "@/types/history-stack";
 
@@ -83,15 +83,125 @@ function LayerInfoPanel({ layer, isSelected }: { layer: Layer, isSelected: boole
   );
 }
 
+const LayerItemRow = memo(function({
+  layer,
+  index,
+  isSelected,
+  isDraggedOver,
+  isExpanded,
+  onClick,
+  onToggleLock,
+  onToggleHide,
+  onRemove,
+  onDragStart,
+  onDragOver,
+  onDrop,
+  onExpand,
+} : {
+  layer: Layer,
+  index: number,
+  isSelected: boolean,
+  isDraggedOver: boolean,
+  isExpanded: boolean,
+  onClick: any,
+  onToggleLock: any,
+  onToggleHide: any,
+  onRemove: any,
+  onDragStart: any,
+  onDragOver: any,
+  onDrop: any,
+  onExpand: any,
+}) {
+  let layerIcon: JSX.Element;
+  switch (layer.type) {
+    case "rectangle":
+      layerIcon = <Square size={16} />;
+      break;
+    case "circle":
+      layerIcon = <Circle size={16} />;
+      break;
+    case "arrow":
+      layerIcon = <ArrowRight size={16} />;
+      break;
+    default:
+      layerIcon = <Square size={16} />;
+      break;
+  }
+
+  return (
+    <div
+      className={cn("border-b border-slate-500", isDraggedOver && "border-t-2 border-t-slate-300")}
+      draggable={true}
+      onDragStart={(e) => onDragStart(e, index)}
+      onDragOver={(e) => onDragOver(e, index)}
+      onDrop={(e) => onDrop(e, index)}
+    >
+      <button
+        className={cn(
+          "flex flex-row justify-between items-center p-3 w-full bg-slate-700 cursor-pointer",
+          isSelected && "bg-slate-800",
+        )}
+        onClick={(e) => onClick(e, layer.uuid)}
+      >
+        <div className="flex flex-row items-center gap-2 text-sm">
+          {layerIcon!}
+          {index + " " + layer.type + "_" + layer.uuid.slice(0,5)}
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <div
+            className="cursor-pointer hover:bg-slate-600 p-1 rounded"
+            onClick={(e) => onExpand(e, layer.uuid)}
+            title="Toggle layer info"
+          >
+            {isExpanded ? <ChevronUp size={16} /> : <ChevronDown size={16} />}
+          </div>
+          <div
+            className="cursor-pointer hover:bg-slate-600 p-1 rounded"
+            onClick={(e) => onToggleLock(e, index)}
+            title={layer.isPinned ? "Unpin layer" : "Pin layer"}
+          >
+            {layer.isPinned ? (
+              <PinOff size={16} />
+            ) : (
+              <Pin size={16} />
+            )}
+          </div>
+          <div
+            className="cursor-pointer hover:bg-slate-600 p-1 rounded"
+            onClick={(e) => onToggleHide(e, index)}
+            title={layer.isHidden ? "Show layer" : "Hide layer"}
+          >
+            {layer.isHidden ? (
+              <EyeOff size={16} />
+            ) : (
+              <Eye size={16} />
+            )}
+          </div>
+          <div
+            className="cursor-pointer hover:bg-slate-600 p-1 rounded"
+            onClick={(e) => onRemove(e, index)}
+            title="Remove layer"
+          >
+            <Trash2 size={16} />
+          </div>
+        </div>
+      </button>
+
+      {isExpanded && (
+        <LayerInfoPanel layer={layer} isSelected={isSelected} />
+      )}
+    </div>
+  );
+});
+
 export default function LayersPane() {
   const { layers, setLayers } = useContext(LayersContext);
   const { inspectingLayerId, setInspectingLayerId } = useContext(PresentationContext);
   const { setSlideHistory } = useContext(HistoryContext);
-
-  const [expandedLayers, setExpandedLayers] = useState<Record<string, boolean>>({});
+  const layersPaneRef = useRef<HTMLDivElement | null>(null);
   const [dragStartIndex, setDragStartIndex] = useState<number | null>(null);
   const [draggedOverIndex, setDraggedOverIndex] = useState<number | null>(null);
-  const layersPaneRef = useRef<HTMLDivElement | null>(null);
+  const [expandedLayers, setExpandedLayers] = useState<Record<string, boolean>>({});
 
   useEffect(() => {
     // Scroll to the bottom of the layers pane when new layers are added
@@ -100,15 +210,12 @@ export default function LayersPane() {
     }
   }, [layers]);
 
-  const toggleLayerInfo = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, layerId: string) => {
+  const handleSelectRow = useCallback((e: any, layerId: string) => {
     e.stopPropagation();
-    setExpandedLayers(prev => ({
-      ...prev,
-      [layerId]: !prev[layerId]
-    }));
-  };
+    setInspectingLayerId(layerId);
+  }, []);
 
-  const toggleLockLayer = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
+  const toggleLockLayer = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
     e.stopPropagation(); // Prevent the click from propagating to the button's onClick
     const isPinned = layers[index].isPinned;
 
@@ -129,9 +236,9 @@ export default function LayersPane() {
       };
       return newLayers;
     });
-  };
+  }, [layers]);
 
-  const toggleHideLayer = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
+  const toggleHideLayer = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
     e.stopPropagation();
     const isHidden = layers[index].isHidden;
 
@@ -152,9 +259,9 @@ export default function LayersPane() {
       };
       return newLayers;
     });
-  };
+  }, [layers]);
 
-  const removeLayer = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
+  const removeLayer = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
     e.stopPropagation();
     setSlideHistory(prev => {
         const newSlideHistory = prev.copy();
@@ -173,16 +280,26 @@ export default function LayersPane() {
         order: i,
       }));
     });
-  };
+  }, [layers]);
 
-  const handleDragStart = (e: React.DragEvent<HTMLDivElement>, index: number) => {
+  const toggleLayerInfo = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>, layerId: string) => {
+    e.stopPropagation();
+    setExpandedLayers(prev => ({
+      ...prev,
+      [layerId]: !prev[layerId]
+    }));
+  }, []);
+
+  const handleDragStart = useCallback((e: React.DragEvent<HTMLDivElement>, index: number) => {
     setDragStartIndex(index);
-  };
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>, index: number) => {
+  }, []);
+
+  const handleDragOver = useCallback((e: React.DragEvent<HTMLDivElement>, index: number) => {
     e.preventDefault();
     setDraggedOverIndex(index);
-  };
-  const handleDrop = (e: React.DragEvent<HTMLDivElement>, index: number) => {
+  }, []);
+
+  const handleDrop = useCallback((e: React.DragEvent<HTMLDivElement>, index: number) => {
     e.preventDefault();
 
     setSlideHistory(prev => {
@@ -203,103 +320,51 @@ export default function LayersPane() {
     });
     setDragStartIndex(null);
     setDraggedOverIndex(null);
-  };
+  }, [dragStartIndex, setLayers, setSlideHistory]);
 
   return (
     <>
-    <p className="text-white text-2xl m-2">Layers</p>
-    <div className="overflow-y-auto h-full" ref={layersPaneRef}> {/* Add h-full here and keep overflow-y-auto */}
-    {layers.map((layer, index) => {
-      let layerIcon: JSX.Element;
-      switch (layer.type) {
-        case "rectangle":
-          layerIcon = <Square size={16} />;
-          break;
-        case "circle":
-          layerIcon = <Circle size={16} />;
-          break;
-        case "arrow":
-          layerIcon = <ArrowRight size={16} />;
-          break;
-        default:
-          layerIcon = <Square size={16} />;
-          break;
-      }
-
-      const isExpanded = expandedLayers[layer.uuid] || false;
-      const isSelected = inspectingLayerId === layer.uuid;
-
-      return (
-        <div
-          key={layer.uuid}
-          className={cn("border-b border-slate-500", draggedOverIndex === index && "border-t-2 border-t-slate-300")}
-          draggable={true}
-          onDragStart={(e) => handleDragStart(e, index)}
-          onDragOver={(e) => handleDragOver(e, index)}
-          onDrop={(e) => handleDrop(e, index)}
-        >
-          <button
-            className={cn(
-              "flex flex-row justify-between items-center p-3 w-full bg-slate-700 cursor-pointer",
-              isSelected && "bg-slate-800",
-            )}
-            onClick={(e) => {
-              e.stopPropagation();
-              setInspectingLayerId(layer.uuid);
-            }}
-          >
-            <div className="flex flex-row items-center gap-2 text-sm">
-              {layerIcon!}
-              {index + " " + layer.type + "_" + layer.uuid.slice(0,5)}
-            </div>
-            <div className="flex flex-row items-center gap-2">
-              <div
-                className="cursor-pointer hover:bg-slate-600 p-1 rounded"
-                onClick={(e) => toggleLayerInfo(e, layer.uuid)}
-                title="Toggle layer info"
-              >
-                {isExpanded ? <ChevronUp size={16} /> : <ChevronDown size={16} />}
-              </div>
-              <div
-                className="cursor-pointer hover:bg-slate-600 p-1 rounded"
-                onClick={(e) => toggleLockLayer(e, index)}
-                title={layer.isPinned ? "Unpin layer" : "Pin layer"}
-              >
-                {layer.isPinned ? (
-                  <PinOff size={16} />
-                ) : (
-                  <Pin size={16} />
-                )}
-              </div>
-              <div
-                className="cursor-pointer hover:bg-slate-600 p-1 rounded"
-                onClick={(e) => toggleHideLayer(e, index)}
-                title={layer.isHidden ? "Show layer" : "Hide layer"}
-              >
-                {layer.isHidden ? (
-                  <EyeOff size={16} />
-                ) : (
-                  <Eye size={16} />
-                )}
-              </div>
-              <div
-                className="cursor-pointer hover:bg-slate-600 p-1 rounded"
-                onClick={(e) => removeLayer(e, index)}
-                title="Remove layer"
-              >
-                <Trash2 size={16} />
-              </div>
-            </div>
-          </button>
-
-          {/* Render layer info panel when expanded */}
-          {isExpanded && (
-            <LayerInfoPanel layer={layer} isSelected={isSelected} />
-          )}
-        </div>
-      );
-    })}
-    </div>
+      <p className="text-white text-2xl m-2">Layers</p>
+      <div className="overflow-y-auto h-full" ref={layersPaneRef}>
+        {layers.map((layer, index) =>
+          <LayerItemRow
+            key={layer.uuid}
+            layer={layer}
+            index={index}
+            onClick={handleSelectRow}
+            onToggleLock={toggleLockLayer}
+            onToggleHide={toggleHideLayer}
+            onRemove={removeLayer}
+            onDragStart={handleDragStart}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+            onExpand={toggleLayerInfo}
+            isSelected={inspectingLayerId === layer.uuid}
+            isDraggedOver={draggedOverIndex === index}
+            isExpanded={expandedLayers[layer.uuid] || false}
+          />
+          // <Test
+          //   key={layer.uuid}
+          //   layer={layer}
+          //   index={index}
+          //   onClick={handleSelectRow}
+          //   isSelected={inspectingLayerId === layer.uuid}
+          // />
+        )}
+      </div>
     </>
   )
 }
+
+const Test = memo(function ({layer, index, onClick, isSelected}: {layer: Layer, index: number, onClick: any, isSelected: boolean}) {
+  console.log("Test component rendered for layer", layer.uuid, "index", index, "isSelected", isSelected);
+  return (
+    <div
+      className={cn(isSelected ? "bg-slate-800" : "bg-slate-700", "p-3 cursor-pointer hover:bg-slate-600")}
+      onClick={(e) => onClick(e, layer.uuid)}
+    >
+      <Square size={16} className="inline-block mr-2" />
+      {layer.type} {index} {layer.uuid.slice(0,5)}
+    </div>
+  );
+});
